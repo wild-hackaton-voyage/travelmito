613e67778dd1a3f883b5736887d0c4060dbdda37
roketag33 <roketag33@gmail.com>
2022-11-24 16:26:18 +0100

    add navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
-
+import './Navbar.css';
 const Navbar = () => {
     return (
-        <div>
-            
+        <div className="navbar-container" >
+            <ul>
+                <li>mitocard</li>
+                <li>mitoselfie</li>
+                <li>mitoticket</li>
+            </ul>
         </div>
-    )
+    );
 }
 
 export default Navbar
