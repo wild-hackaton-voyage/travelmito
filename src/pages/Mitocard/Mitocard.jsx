1694c8a59e8de20f80c5dd797544002b75a0c3b8
Mbirepinte <mickael.birepinte@gmail.com>
2022-11-25 00:24:43 +0100

    carousel de carte marche

--- src/pages/Mitocard/Mitocard.jsx
+++ src/pages/Mitocard/Mitocard.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 import { useParams } from "react-router-dom";
 import Cartes from "../../components/Navbar/Cartes";
 import Navbar from "../../components/Navbar/Navbar";
@@ -46,14 +47,30 @@ const Mitocard = ({ handleDestination, destination }) => {
           <Carousel responsive={responsive} >
             <img 
             className="postcard"    
-            src="Cartes/France/France1.jpeg"
-            alt = "France1"
-            />    
+            src={`/Cartes/${destination}/${destination}1.jpeg`}
+            alt = {`${destination}1`}
+            /> 
             <img 
             className="postcard"    
-            src="././France/France1.jpeg"
-            alt = "France1"
-          /> 
+            src={`/Cartes/${destination}/${destination}2.jpeg`}
+            alt = {`${destination}2`}
+            /> 
+            <img 
+            className="postcard"    
+            src={`/Cartes/${destination}/${destination}3.jpeg`}
+            alt = {`${destination}3`}
+            />  
+            <img 
+            className="postcard"    
+            src={`/Cartes/${destination}/${destination}4.jpeg`}
+            alt = {`${destination}4`}
+            />  
+        <img 
+            className="postcard"    
+            src={`/Cartes/${destination}/${destination}5.jpeg`}
+            alt = {`${destination}5`}
+            />  
+
           </Carousel>
     </div>
   );
