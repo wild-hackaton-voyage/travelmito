00ec4600cc67161fed1c7dc7b1fb4d9344584c47
Mbirepinte <mickael.birepinte@gmail.com>
2022-11-24 16:32:05 +0100

    import spline

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ const App = () => {
   return (
     <div>
       <h1>TravelMito</h1>
-      
+      <iframe className="globe" src='https://my.spline.design/world-41f0da7ed9dbbeffa52ee11a75785759/' frameborder='0' width='100%' height='100%'></iframe>
     </div>
   );
 };
--- src/pages/Mitocard/Mitocard.jsx
+++ src/pages/Mitocard/Mitocard.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import Carousel from 'react-multi-carousel';
+import Cartes from '../../components/Navbar/Cartes';
 
 const Mitocard = () => {
     return (
-        <div>
+        <div>   
+            <div className="carousel">
+                <Carousel>
+                    <Cartes/>
+                </Carousel>
+
+
+            </div>
             
         </div>
     );
