94d7116eee243ce218179d607805d9ee5c213867
roketag33 <roketag33@gmail.com>
2022-11-25 00:25:54 +0100

    ca se lance enfin

--- src/pages/Mitoselfie/Mitoselfie.jsx
+++ src/pages/Mitoselfie/Mitoselfie.jsx
@@ -1,15 +1,50 @@
-import React from 'react';
+import{ React, useEffect} from 'react';
 import Navbar from "../../components/Navbar/Navbar";
+import Carousel from "react-multi-carousel";
+import { useParams } from "react-router-dom";
+import "./mitoselfie.css";
 
-const Mitoselfie = ({destination}) => {
 
-    console.log(destination);
+const responsive = {
+
+    superLargeDesktop: {
+      breakpoint: { max: 4000, min: 3000 },
+      items: 3,
+      partialVisibilityGutter: 40,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3,
+      partialVisibilityGutter: 40,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 3,
+      partialVisibilityGutter: 40,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 3,
+      partialVisibilityGutter: 40,
+    },
+  };
+
+  const Mitoselfie = ({ handleDestination, destination }) => {
+    const { id } = useParams();
+
+    useEffect(() => {
+      handleDestination(id);
+    }, [id]);
+
     return (
-        <div>
-            <Navbar destination={destination}/>
-            {destination} salut
+        <div className='mitoselfie'>
+            <p>cv</p>
+        <div className="carousel">
+          <Navbar destination={destination} />
+          {destination} salut
         </div>
+      </div>
     );
-};
+  };
 
 export default Mitoselfie;
