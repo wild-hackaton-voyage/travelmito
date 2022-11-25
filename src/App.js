e635f54ded3dd5ae792e3dd9deb7249985bc9528
theo anache <theoo.anache@gmail.com>
2022-11-25 12:04:21 +0100

    test logo bottom

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
 import Transition from "./components/Navbar/Transition";
+import LogoCache from "./asssets/photo/logo-cache-spline.jpg";
 
 const App = () => {
   return (
     <div className="ticketContainer">
+      <div className="logoBottom">
+        <img src={LogoCache} alt={LogoCache} />
+      </div>
       <Router>
         <Transition />
       </Router>
